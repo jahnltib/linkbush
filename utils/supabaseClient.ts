7d89bd450116bcb3bcb7606c3daca35968d00571
jahnltib <jahnltib+github@gmail.com>
2024-07-27 13:52:10 -0700

    removed incorrect files

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "https://qgidmhkuhyejuzgkmiio.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnaWRtaGt1aHllanV6Z2ttaWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxMTA3MjYsImV4cCI6MjAzNzY4NjcyNn0.aB7bBOPF6DYcaEuqP90q62GlHuEHIo2-8Us_jdvsxtQ";
 
 const supabase = createClient (supabaseUrl, supabaseAnonKey);
 export default supabase;
+
